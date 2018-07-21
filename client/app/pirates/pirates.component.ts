ba583e3f841e9c7da77989b0e2a4decc4ca554af
Sergey Moiseenko <sergeymoiseenko.i@mail.ru>
2018-07-21 22:04:28 +0800

    Добавил удобную подгрузку ресурсов

--- client/app/pirates/pirates.component.ts
+++ client/app/pirates/pirates.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { background } from './Utils';
+import { background, IMGS, loadResources } from './Utils';
 
 declare var PIXI: any;
 
@@ -20,26 +20,26 @@ export class PiratesComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    const containerSize = {x: 1110, y: 624};
+
     setTimeout( () => {
-      const app = new PIXI.Application(containerSize.x, containerSize.y, {backgroundColor : 0x1099bb});
-      this.container.nativeElement.appendChild(app.view);
-
-
-
-      const renderer = app.renderer;
-      const container = new PIXI.Container();
-
-      app.stage.addChild(container);
-
-      PIXI.loader.add('assets/img/bg_ocean_middle.png').load(function () {
-          const slide = background(containerSize, new PIXI.Sprite.fromImage('assets/img/bg_ocean_middle.png'), 'cover');
-          container.addChild(slide);
-
-          renderer.render(app.stage);
-      });
-
+      loadResources(this.runGame.bind(this));
     }, 0);
+  }
+
+  runGame() {
+    const containerSize = {x: 1110, y: 624};
+    const app = new PIXI.Application(containerSize.x, containerSize.y, {backgroundColor : 0x1099bb});
+    this.container.nativeElement.appendChild(app.view);
+
+    const renderer = app.renderer;
+    const container = new PIXI.Container();
+
+    app.stage.addChild(container);
+
+    const slide = background(containerSize, new PIXI.Sprite(IMGS.BACKGROUND_SEA), 'cover');
+    container.addChild(slide);
+
+    renderer.render(app.stage);
   }
 
 }
